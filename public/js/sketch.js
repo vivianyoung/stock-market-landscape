f12102796dbbf8a41df462920b8da581f9d85898
vivian young <vivian.young.2000@gmail.com>
2021-01-09 12:23:15 -0600

    Set up initial p5.js canvas on home page

---
+++ public/js/sketch.js
@@ -0,0 +1,16 @@
+var screenWidth;
+var screenHeight;
+
+function setup() {
+  // Initializing the screen's width and height
+  screenWidth = innerWidth;
+  screenHeight = innerHeight;
+
+  // Initializing the canvas and the graph
+  createCanvas(screenWidth,screenHeight);
+  background(25);
+}
+
+function draw() {
+
+}
